0b7fcd3f99c15d50403d23e660618cb0f6deaeeb
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-11-24 12:32:25 +0300

    Add some OneMaxPerm-related unit tests.

--- src/test/scala/ru/ifmo/onell/algorithm/OnePlusLambdaLambdaGATests.scala
+++ src/test/scala/ru/ifmo/onell/algorithm/OnePlusLambdaLambdaGATests.scala
@@ -4,7 +4,7 @@ import scala.Ordering.Double.IeeeOrdering
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import ru.ifmo.onell.problem.OneMax
+import ru.ifmo.onell.problem.{OneMax, OneMaxPerm}
 
 class OnePlusLambdaLambdaGATests extends FlatSpec with Matchers {
   "(1+LL) GA" should "perform as expected on OneMax" in {
@@ -14,5 +14,14 @@ class OnePlusLambdaLambdaGATests extends FlatSpec with Matchers {
     val runs = IndexedSeq.fill(100)(ll.optimize(om))
     val found = runs.sum.toDouble / runs.size
     found should (be <= 1300.0)
+  }
+
+  it should "perform as expected on OneMaxPerm" in {
+    val size = 200
+    val om = new OneMaxPerm(size)
+    val ll = new OnePlusLambdaLambdaGA(OnePlusLambdaLambdaGA.logCappedAdaptiveLambda)
+    val runs = IndexedSeq.fill(10)(ll.optimize(om))
+    val found = runs.sum.toDouble / runs.size
+    found should (be <= 1.1e5)
   }
 }
--- src/test/scala/ru/ifmo/onell/algorithm/OnePlusOneEATests.scala
+++ src/test/scala/ru/ifmo/onell/algorithm/OnePlusOneEATests.scala
@@ -3,7 +3,8 @@ package ru.ifmo.onell.algorithm
 import scala.Ordering.Double.IeeeOrdering
 
 import org.scalatest.{FlatSpec, Matchers}
-import ru.ifmo.onell.problem.OneMax
+
+import ru.ifmo.onell.problem.{OneMax, OneMaxPerm}
 
 class OnePlusOneEATests extends FlatSpec with Matchers {
   "(1+1) EA" should "perform as expected on OneMax" in {
@@ -11,6 +12,15 @@ class OnePlusOneEATests extends FlatSpec with Matchers {
     val om = new OneMax(size)
     val runs = IndexedSeq.fill(100)(OnePlusOneEA.optimize(om))
     val expected = size * (1 to size / 2).map(1.0 / _).sum * (math.E - 1)
+    val found = runs.sum.toDouble / runs.size
+    found should (be <= expected)
+  }
+
+  it should "perform as expected on OneMaxPerm" in {
+    val size = 200
+    val om = new OneMaxPerm(size)
+    val runs = IndexedSeq.fill(10)(OnePlusOneEA.optimize(om))
+    val expected = size / 2.0 * size * (1 to size / 2).map(1.0 / _).sum * (math.E - 1)
     val found = runs.sum.toDouble / runs.size
     found should (be <= expected)
   }
