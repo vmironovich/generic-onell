e1b5c215d10e6bce930068338e12e019e06765bb
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-12-03 21:54:30 +0300

    Add permutation test to RLSTests.

--- src/test/scala/ru/ifmo/onell/algorithm/RLSTests.scala
+++ src/test/scala/ru/ifmo/onell/algorithm/RLSTests.scala
@@ -1,9 +1,8 @@
 package ru.ifmo.onell.algorithm
 
 import scala.Ordering.Double.IeeeOrdering
-
 import org.scalatest.{FlatSpec, Matchers}
-import ru.ifmo.onell.problem.OneMax
+import ru.ifmo.onell.problem.{OneMax, OneMaxPerm}
 
 class RLSTests extends FlatSpec with Matchers {
   "RLS" should "perform as expected on OneMax" in {
@@ -13,5 +12,14 @@ class RLSTests extends FlatSpec with Matchers {
     val expected = size * (1 to size / 2).map(1.0 / _).sum
     val found = runs.sum.toDouble / runs.size
     found should (be <= expected * 1.1)
+  }
+
+  it should "perform as expected on OneMaxPerm" in {
+    val size = 200
+    val om = new OneMaxPerm(size)
+    val runs = IndexedSeq.fill(20)(RLS.optimize(om))
+    val expected = size / 2.0 * size * (1 to size / 2).map(1.0 / _).sum
+    val found = runs.sum.toDouble / runs.size
+    found should (be <= expected * 1.2)
   }
 }
