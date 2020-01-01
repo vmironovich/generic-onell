df0d335b53be8c2c27260ceafe7517933b641284
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-01-02 00:54:26 +0300

    Add a rudimentary suite for generating Bassin-like 3D plots.

--- src/main/scala/ru/ifmo/onell/Main.scala
+++ src/main/scala/ru/ifmo/onell/Main.scala
@@ -4,11 +4,11 @@ import java.io.PrintWriter
 import java.util.Random
 
 import scala.util.Using
+import scala.Ordering.Double.IeeeOrdering
 
 import ru.ifmo.onell.algorithm.{OnePlusLambdaLambdaGA, OnePlusOneEA, RLS}
-import ru.ifmo.onell.problem.{LinearRandomDoubleWeights, OneMax, OneMaxPerm, RandomPlanted3SAT}
+import ru.ifmo.onell.problem.{LinearRandomDoubleWeights, LinearRandomIntegerWeights, OneMax, OneMaxPerm, RandomPlanted3SAT}
 import ru.ifmo.onell.util.par._
-
 import ru.ifmo.onell.algorithm.OnePlusLambdaLambdaGA._
 
 object Main {
@@ -129,6 +129,63 @@ object Main {
     }
   }
 
+  private class HammingImprovementCollector(size: Int) extends IterationLogger[LinearRandomIntegerWeights.FAHD] {
+    private[this] val hammingCounts, hammingIncrements = new Array[Long](size + 1)
+    private[this] var lastEvaluations, lastFitness = 0L
+    private[this] var lastDistance = -1
+
+    override def logIteration(evaluations: Long, fitness: LinearRandomIntegerWeights.FAHD): Unit = {
+      if (evaluations == 1) { // start
+        lastEvaluations = 1
+        lastDistance = fitness.distance
+        lastFitness = fitness.fitness
+      } else {
+        if (fitness.fitness > lastFitness) {
+          hammingCounts(lastDistance) += evaluations - lastEvaluations
+          hammingIncrements(lastDistance) += lastDistance - fitness.distance
+          lastEvaluations = evaluations
+          lastDistance = fitness.distance
+          lastFitness = fitness.fitness
+        }
+      }
+    }
+
+    def extract(target: Array[Double]): Unit = {
+      for (i <- 1 to size / 2) {
+        target(i) = hammingIncrements(i).toDouble / hammingCounts(i)
+      }
+    }
+  }
+
+  private def collect3DPlots(weight: Int): Unit = {
+    Using.resource(new PrintWriter("dump.txt")) { out =>
+      val rng = new Random(9234352524211L)
+      val n = 1000
+      val runs = 1000
+      val arrays = Array.ofDim[Double](100, n / 2 + 1)
+      def lambdaGenFun(lg: Int): Double = math.pow(1.05, lg)
+
+      for (lambdaGen <- arrays.indices; lambda = lambdaGenFun(lambdaGen)) {
+        val fun = new LinearRandomIntegerWeights(n, weight, rng.nextLong())
+        val oll = new OnePlusLambdaLambdaGA(fixedLambda(lambda))
+        val logger = new HammingImprovementCollector(n)
+        for (_ <- 0 until runs) oll.optimize(fun, logger)
+        logger.extract(arrays(lambdaGen))
+        println(s"lambda $lambda done")
+      }
+      for (dist <- 1 to n / 2) {
+        val sumAcross = arrays.view.map(_(dist)).max
+        arrays.foreach(_(dist) /= sumAcross)
+      }
+      for (lambdaGen <- arrays.indices; lambda = lambdaGenFun(lambdaGen)) {
+        for (dist <- 1 to n / 2) {
+          out.println(s"$dist $lambda ${arrays(lambdaGen)(dist)}")
+        }
+        out.println()
+      }
+    }
+  }
+
   private implicit class Options(val args: Array[String]) extends AnyVal {
     def getOption(option: String): String = {
       val index = args.indexOf(option)
@@ -153,6 +210,7 @@ object Main {
       case "bits:l2d:simple" => bitsLinearDoubleSimple(parseContext(args))
       case "bits:sat:simple" => bitsMaxSATSimple(parseContext(args))
       case "perm:om:simple"  => permOneMaxSimple(parseContext(args))
+      case "3d" => collect3DPlots(args(1).toInt)
       case _ => usage()
     }
   }
