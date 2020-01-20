0d66c0b2b43b2b46535b8df9c99722f1aebd9a4f
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-01-20 05:24:02 +0300

    Add code that tests how precise the upper bounds are.

--- src/main/scala/ru/ifmo/onell/main/FixedTarget.scala
+++ src/main/scala/ru/ifmo/onell/main/FixedTarget.scala
@@ -2,6 +2,8 @@ package ru.ifmo.onell.main
 
 import java.util.concurrent.ThreadLocalRandom
 
+import scala.Ordering.Double.TotalOrdering
+
 import ru.ifmo.onell.algorithm.OnePlusOneEA
 import ru.ifmo.onell.problem.OneMax
 import ru.ifmo.onell.{HasIndividualOperations, IterationLogger, Main}
@@ -26,7 +28,7 @@ object FixedTarget extends Main.Module {
     val fineStep = args.getOption("--fine-step").toInt
     val runs = args.getOption("--runs").toInt
 
-    val collectorZero, collectorMean = new FixedTargetLogger(n)
+    val collectorZero, collectorRand = new FixedTargetLogger(n)
     val oneMax = new OneMax(n)
 
     locally {
@@ -34,43 +36,62 @@ object FixedTarget extends Main.Module {
       (0 until runs).foreach(_ => OnePlusOneEA.PracticeUnaware.optimize(oneMax, collectorZero))
     }
     locally {
-      (0 until runs).foreach(_ => OnePlusOneEA.PracticeUnaware.optimize(oneMax, collectorMean))
+      (0 until runs).foreach(_ => OnePlusOneEA.PracticeUnaware.optimize(oneMax, collectorRand))
     }
 
     val harmonic = new Array[Double](n + 1)
     for (i <- 1 to n) {
       harmonic(i) = harmonic(i - 1) + 1.0 / i
     }
+
+    val halfHarmonic = if (n % 2 == 0) harmonic(n / 2) else (harmonic(n / 2) + harmonic(n - n / 2)) / 2
     val secondBoundStart = n / 2.0 + math.sqrt(n) * math.log(n)
+
+    def upperZero(i: Int): Double = math.max(1, math.E * n * (harmonic(n) - harmonic(n - i)))
+    def upperRand(i: Int): Double = math.max(1, math.E * n * (halfHarmonic - harmonic(n - i)))
+
+    val thresholdZero15, thresholdRand15 = new ThresholdTracker(1.5)
+    val thresholdZero25 = new ThresholdTracker(2.5)
+
+    val pairsZero = (0 to n).map(i => (i, upperZero(i) / collectorZero.get(i, runs)))
+    val maxValZero = pairsZero.maxBy(_._2)
+    pairsZero.foreach((thresholdZero15.accept _).tupled)
+    pairsZero.foreach((thresholdZero25.accept _).tupled)
+
+    val pairsRand = (0 to n).map(i => (i, upperRand(i) / collectorRand.get(i, runs)))
+    val maxValRand = pairsRand.maxBy(_._2)
+    pairsRand.foreach((thresholdRand15.accept _).tupled)
+
+    println(s"For zero at 2.5: maximum $maxValZero, ${thresholdZero25.report}")
+    println(s"For zero at 1.5: ${thresholdZero15.report}")
+    println(s"For rand at 1.5: maximum $maxValRand, ${thresholdRand15.report}")
+    println()
 
     for (stepper <- Seq(0 to n by step, fineStart to n by fineStep)) {
       print("\\addplot+ coordinates {")
       for (i <- stepper) {
-        val upper = math.max(1, math.E * n * (harmonic(n) - harmonic(n - i)))
-        print(s"(${i.toDouble / n},$upper)")
+        print(s"(${i.toDouble / n},${upperZero(i)}")
       }
       println("};")
       println("\\addlegendentry{Upper bound~\\cite{practice-aware}};")
 
       print("\\addplot+ plot[error bars/.cd, y dir=both, y explicit] coordinates {")
       for (i <- stepper) {
-        print(collectorZero.get(i, runs))
+        print(collectorZero.getStr(i, runs))
       }
       println("};")
       println("\\addlegendentry{$(1+1)$ EA from zero};")
 
       print("\\addplot+ coordinates {")
-      val halfHarmonic = if (n % 2 == 0) harmonic(n / 2) else (harmonic(n / 2) + harmonic(n - n / 2)) / 2
       for (i <- stepper if i > secondBoundStart) {
-        val upper = math.max(1, math.E * n * (halfHarmonic - harmonic(n - i)))
-        print(s"(${i.toDouble / n},$upper)")
+        print(s"(${i.toDouble / n},${upperRand(i)})")
       }
       println("};")
       println("\\addlegendentry{Upper bound~\\cite{fixed-target-gecco19}};")
 
       print("\\addplot+ plot[error bars/.cd, y dir=both, y explicit] coordinates {")
       for (i <- stepper) {
-        print(collectorMean.get(i, runs))
+        print(collectorRand.getStr(i, runs))
       }
       println("};")
       println("\\addlegendentry{$(1+1)$ EA from mean};")
@@ -91,7 +112,9 @@ object FixedTarget extends Main.Module {
     private[this] val collectorSq = new Array[Double](problemSize + 1)
     private[this] var lastFitness = -1
 
-    def get(index: Int, runs: Int): String = {
+    def get(index: Int, runs: Int): Double = collector(index).toDouble / runs
+
+    def getStr(index: Int, runs: Int): String = {
       val avg = collector(index).toDouble / runs
       val std = math.sqrt((collectorSq(index) / runs - avg * avg) * runs / (runs - 1))
       s"(${index.toDouble / problemSize},$avg)+-(0,$std)"
@@ -121,6 +144,25 @@ object FixedTarget extends Main.Module {
       if (index < 0) throw new IllegalArgumentException(s"No option '$option' is given")
       if (index + 1 == args.length) throw new IllegalArgumentException(s"Option '$option' should have an argument")
       args(index + 1)
+    }
+  }
+
+  private class ThresholdTracker(threshold: Double) {
+    private[this] var minIndex, minIndexEver = Int.MaxValue
+    private[this] var maxIndex = Int.MinValue
+    private[this] var count = 0
+
+    def accept(index: Int, value: Double): Unit = {
+      minIndexEver = math.min(minIndexEver, index)
+      if (value >= threshold) {
+        maxIndex = math.max(maxIndex, index)
+        minIndex = math.min(minIndex, index)
+        count += 1
+      }
+    }
+
+    def report: String = {
+      s"min index = $minIndex >= $minIndexEver, max index = $maxIndex, count = $count <= ${maxIndex - minIndex + 1}"
     }
   }
 
