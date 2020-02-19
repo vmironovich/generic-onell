e1a76063bd831dbfb396c9929dc85a0d17fd7089
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-02-19 03:11:44 +0300

    Change integer distribution signature, add extensions.

--- src/main/scala/ru/ifmo/onell/IntegerDistribution.scala
+++ src/main/scala/ru/ifmo/onell/IntegerDistribution.scala
@@ -2,10 +2,25 @@ package ru.ifmo.onell
 
 import java.util.Random
 
+import scala.annotation.tailrec
+
 trait IntegerDistribution {
   def sample(rng: Random): Int
 }
 
 object IntegerDistribution {
   def constant(value: Int): IntegerDistribution = _ => value
+
+  implicit class Extensions(val underlying: IntegerDistribution) extends AnyVal {
+    def max(other: IntegerDistribution): IntegerDistribution = r => math.max(underlying.sample(r), other.sample(r))
+    def skipUntil(predicate: Int => Boolean): IntegerDistribution = new SkipUntil(underlying, predicate)
+  }
+
+  private class SkipUntil(distribution: IntegerDistribution, predicate: Int => Boolean) extends IntegerDistribution {
+    @tailrec
+    override final def sample(rng: Random): Int = {
+      val current = distribution.sample(rng)
+      if (predicate(current)) current else sample(rng)
+    }
+  }
 }
--- src/main/scala/ru/ifmo/onell/util/BinomialDistribution.scala
+++ src/main/scala/ru/ifmo/onell/util/BinomialDistribution.scala
@@ -5,9 +5,9 @@ import java.util.Random
 import ru.ifmo.onell.IntegerDistribution
 
 object BinomialDistribution {
-  def apply(n: Int, p: Double): IntegerDistribution = rng => withScanner(n, p, rng)
+  def apply(n: Long, p: Double): IntegerDistribution = rng => withScanner(n, p, rng)
 
-  private def withScanner(n: Int, p: Double, rng: Random): Int = {
+  private def withScanner(n: Long, p: Double, rng: Random): Int = {
     val sc = BinomialScanner(p)
     var idx = sc.offset(rng) - 1
     var result = 0
