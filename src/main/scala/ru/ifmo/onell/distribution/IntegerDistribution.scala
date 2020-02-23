718d553270ac94db900e10ddf88ca717e40cad91
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-02-23 22:55:47 +0300

    Move extension methods into the trait.

--- src/main/scala/ru/ifmo/onell/distribution/IntegerDistribution.scala
+++ src/main/scala/ru/ifmo/onell/distribution/IntegerDistribution.scala
@@ -2,22 +2,20 @@ package ru.ifmo.onell.distribution
 
 import java.util.Random
 
-import scala.annotation.tailrec
-
 trait IntegerDistribution {
+  import IntegerDistribution._
+
   def sample(rng: Random): Int
+
+  def max(other: IntegerDistribution): IntegerDistribution = r => math.max(sample(r), other.sample(r))
+  def takeWhen(predicate: Int => Boolean): IntegerDistribution = new TakeWhen(this, predicate)
 }
 
 object IntegerDistribution {
   def constant(value: Int): IntegerDistribution = _ => value
 
-  implicit class Extensions(val underlying: IntegerDistribution) extends AnyVal {
-    def max(other: IntegerDistribution): IntegerDistribution = r => math.max(underlying.sample(r), other.sample(r))
-    def takeWhen(predicate: Int => Boolean): IntegerDistribution = new TakeWhen(underlying, predicate)
-  }
-
-  private class TakeWhen(distribution: IntegerDistribution, predicate: Int => Boolean) extends IntegerDistribution {
-    @tailrec
+  class TakeWhen(distribution: IntegerDistribution, predicate: Int => Boolean) extends IntegerDistribution {
+    @scala.annotation.tailrec
     override final def sample(rng: Random): Int = {
       val current = distribution.sample(rng)
       if (predicate(current)) current else sample(rng)
