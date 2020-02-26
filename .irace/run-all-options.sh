9e6c18787166948920bb217b34e510eed95835eb
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-02-26 17:45:29 +0300

    Revert an earlier attempt to pass parameters through environment

--- .irace/run-all-options.sh
+++ .irace/run-all-options.sh
@@ -16,8 +16,7 @@ for LAMBDA_TUNING in $1; do
                 for MUTANT_STRATEGY in ignore skip-crossover do-not-count do-not-sample; do
                     for POPSIZE_ROUNDING in round-up round-down probabilistic; do
                         MEANINGFUL_OPTIONS="--lambda-tuning $LAMBDA_TUNING $LAMBDA_ADDITIONAL --mutation-strength-alg $MUTATION_STRENGTH --crossover-strength-alg $CROSSOVER_STRENGTH --crossover-strength-base $CROSSOVER_BASE --good-mutant-strategy $MUTANT_STRATEGY --popsize-rounding $POPSIZE_ROUNDING"
-                        ALL_OPTIONS="$COMMON_OPTIONS $MEANINGFUL_OPTIONS"
-                        export ALL_OPTIONS
+                        echo "$COMMON_OPTIONS $MEANINGFUL_OPTIONS" > params.extra
                         echo "$MEANINGFUL_OPTIONS"
                         $IRACE_EXEC --scenario $SCENARIO | tail -n 4
                         echo ""
