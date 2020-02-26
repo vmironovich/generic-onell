962cafcc8d65d0492cd414d7d29400deb98cd35c
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-02-26 12:57:30 +0300

    Implement a C client with UDP sockets

--- .irace/target-runner.sh
+++ .irace/target-runner.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-SCALA_LIBRARY=~/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.13.1.jar
-
 #DEFAULT="--algorithm oll --lambda-tuning fixed --lambda 8 --mutation-strength-alg shift --crossover-strength-alg shift --crossover-strength-base distance --good-mutant-strategy do-not-sample --popsize-rounding probabilistic"
 DEFAULT="--average-over 25 --algorithm oll --lambda-tuning fixed --lambda 8 --mutation-strength-alg standard --crossover-strength-alg standard --crossover-strength-base lambda --good-mutant-strategy ignore --popsize-rounding round-up"
 
-java -cp ../target/scala-2.13/classes:$SCALA_LIBRARY ru.ifmo.onell.Main irace "$@" $DEFAULT --seed $3
+./target-sender.exe --port 8806 "$@" $DEFAULT --seed $3
---
+++ .irace/target-sender.c
@@ -0,0 +1,99 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 10240
+
+char send_buffer[BUFFER_SIZE + 1];
+char recv_buffer[BUFFER_SIZE + 1];
+
+int main(int argc, char *argv[]) {
+    int i, sum_length = -1, port_number = -1;
+    int socket_id;
+    char *send_buffer_ptr = send_buffer;
+    struct sockaddr_in address;
+    int num_bytes_read, num_bytes_written;
+    int buffer_size = BUFFER_SIZE;
+
+    for (i = 1; i < argc; ++i) {
+        sum_length += 1 + strlen(argv[i]);
+    }
+    if (sum_length > BUFFER_SIZE) {
+        printf("Command length too long: %d\n", sum_length);
+        return 1;
+    }
+    for (i = 1; i < argc; ++i) {
+        if (!strcmp("--port", argv[i]) && i + 1 < argc) {
+            port_number = atoi(argv[i + 1]);
+        }
+        int length = strlen(argv[i]);
+        memcpy(send_buffer_ptr, argv[i], length + 1);
+        if (i + 1 < argc) {
+            send_buffer_ptr[length] = ' ';
+        }
+        send_buffer_ptr += length + 1;
+    }
+
+    if (port_number <= 0) {
+        printf("Port number (--port) was not found or is incorrect: %d\n", port_number);
+        return 1;
+    }
+
+    socket_id = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket_id < 0) {
+        int error = errno;
+        printf("Could not open socket! Error code: %d (%s)\n", error, strerror(error));
+        return 1;
+    }
+
+    if (setsockopt(socket_id, SOL_SOCKET, SO_SNDBUF, &buffer_size, sizeof(buffer_size)) == -1) {
+        int error = errno;
+        printf("Could not set socket's send buffer size! Error code: %d (%s)\n", error, strerror(error));
+        return 1;
+    }
+
+    if (setsockopt(socket_id, SOL_SOCKET, SO_RCVBUF, &buffer_size, sizeof(buffer_size)) == -1) {
+        int error = errno;
+        printf("Could not set socket's receive buffer size! Error code: %d (%s)\n", error, strerror(error));
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons((unsigned) port_number);
+    address.sin_addr.s_addr = INADDR_ANY; // local connections only, sort of
+    if (connect(socket_id, &address, sizeof(address))) {
+        int error = errno;
+        printf("Could not connect socket! Error code: %d (%s)\n", error, strerror(error));
+        close(socket_id);
+        return 1;
+    }
+
+    num_bytes_written = write(socket_id, send_buffer, sum_length);
+    if (num_bytes_written != sum_length) {
+        if (num_bytes_written < 0) {
+            int error = errno;
+            printf("Could not write to the socket! Error code: %d (%s)\n", error, strerror(error));
+        } else {
+            printf("Wrote %d bytes instead of %d, exiting\n", num_bytes_written, sum_length);
+        }
+        close(socket_id);
+        return 1;
+    }
+
+    num_bytes_read = read(socket_id, recv_buffer, BUFFER_SIZE);
+    if (num_bytes_read < 0) {
+        int error = errno;
+        printf("Could not read from the socket! Error code: %d (%s)\n", error, strerror(error));
+        close(socket_id);
+    }
+    recv_buffer[num_bytes_read] = 0;
+    printf("%s\n", recv_buffer);
+    close(socket_id);
+
+    return 0;
+}
